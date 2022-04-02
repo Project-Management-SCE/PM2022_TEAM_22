9212a3e4981336ae54f7f360971e4c6edd7724da
Yuval <yuvalvarshevsky@gmail.com>
2022-04-02 22:31:37 +0300

    set up templates

--- base/views.py
+++ base/views.py
@@ -16,4 +16,10 @@ def home(request):
 
 
 def room(request, pk):
-    return render(request, "base/room.html")
+    room = None
+
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {"room": room}
+    return render(request, "base/room.html", context)
