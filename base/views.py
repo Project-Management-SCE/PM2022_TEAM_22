647aa4241d5a4d71342c0af371c45dec82386024
Yuval <yuvalvarshevsky@gmail.com>
2022-04-04 17:15:00 +0300

    BSPM2022T22-102 BSPM2022T22-32 BSPM2022T22-33 BSPM2022T22-34 BSPM2022T22-35	BSPM2022T22-37 BSPM2022T22-38 BSPM2022T22-39 BSPM2022T22-40

--- base/views.py
+++ base/views.py
@@ -1,25 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Room
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 
-rooms = [
-    {"id": 1, "name": "Lets learn python"},
-    {"id": 2, "name": "Stocks"},
-    {"id": 3, "name": "Bonds"},
-]
+def loginPage(request):
+    page = "login"
+    if request.method == "POST":
+        username = request.POST.get("username").lower()
+        password = request.POST.get("password")
+        try:
+            user = User.objects.get(username=username)
+        except BaseException:
+            messages.error(request, "User does not exist")
+            return render(request, "base/login_register.html")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Username or password isn't valid")
+
+    context = {"page": page}
+    return render(request, "base/login_register.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("home")
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Registeration failed")
+    context = {"form": form}
+    return render(request, "base/login_register.html", context)
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
 
+@login_required(login_url="login")
 def room(request, pk):
-    room = None
-
-    for i in rooms:
-        if i["id"] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {"room": room}
     return render(request, "base/room.html", context)
