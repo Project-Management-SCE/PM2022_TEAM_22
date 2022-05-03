e635e71a3c0381f427b25c9efb6d5a14da9e353b
eytanbab <Eytanba@ac.sce.ac.il>
2022-05-03 13:01:59 +0300

    BSPM2022T22-93 Added 52-weeks high and low price

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("logout/", views.logoutUser, name="logout"),
     # path("upgrade_vip_page/", views.upgrade_vip_page, name="upgrade_vip_page"),
     path("upgrade_vip/", views.upgrade_vip, name="upgrade_vip"),
+    path("upgrade_platinum/", views.upgrade_platinum, name="upgrade_platinum"),
     path("register/", views.registerPage, name="register"),
     path("", views.home, name="home"),
     path("change_password/", views.change_password, name="change_password"),
