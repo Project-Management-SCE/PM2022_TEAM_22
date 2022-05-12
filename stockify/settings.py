4ce474fb2c13f36356d778f4ed010998b3d50342
Yuval <yuvalvarshevsky@gmail.com>
2022-05-12 15:53:17 +0300

    test

--- stockify/settings.py
+++ stockify/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "base.apps.BaseConfig",
     "django.contrib.humanize",
 ]
